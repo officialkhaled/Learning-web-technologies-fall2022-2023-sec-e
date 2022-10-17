bd8a5377b3748ea0b53e880f78953f34c0a36f86
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-10-17 13:34:27 +0600

    Lab-4 uploaded

---
+++ Lab4/picture.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Picture Form</title>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+        <fieldset>
+            <legend>PROFILE PICTURE</legend>
+            <table>
+                <tr>
+                    <td><img src="" alt="pfp"></td>
+                </tr>
+                <tr>
+                    <td><input type="file" name="image" value=""></td>
+                </tr>
+            </table>
+            <hr>
+            <table>
+                <tr>
+                    <td>          
+                        <td><input type="submit" name="" id="Submit"></td>
+                    </td>
+                </tr>
+            </table>
+        </fieldset>
+    </form>
+    </body>
+</html>
+
+<?php
+
+?>
