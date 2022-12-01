d421cbf878473e0ab00bcbf3c8cac41834204e8e
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2022-12-01 18:29:17 +0600

    fixing the search bar

--- Lab8 - AJAX Implementation(Search)/index.php
+++ Lab8 - AJAX Implementation(Search)/index.php
@@ -14,7 +14,7 @@
       <form action="index.php" method="post">
         <input
           type="text"
-          placeholder="Search.."
+          placeholder="Search.." autocomplete="off"
           name="search"
           id="search"
           onkeyup="showHint(this.value)"
@@ -33,18 +33,20 @@
 
     </div>
 
-    
-
     <script>
 
       function searchAjax() {
         var search = document.getElementById("search").value;
         var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "index.php?q=" + search, true);
+        
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        xhttp.open("POST", "index.php?q=" + search, true);
         xhttp.send();
+
         xhttp.onreadystatechange = function () {
           if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("txtHint").innerHTML = this.responseText;
+            document.getElementById("txtHint")[0].innerHTML = this.responseText;
           }
         };
         
